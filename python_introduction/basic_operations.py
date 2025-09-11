ae29c3f47fc95c1e8f83bff301ffeb59cef04fd1
Ajumoke-1isiodu <georgiaisiodu@gmail.com>
2025-09-11 14:28:26 +0100

    update corrections

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,14 @@
+# Define variables
+number1 = 10
+number2 = 5
 
-number1=10 
-number2=5
+# Perform operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 
-sum(number1 + number2)
-Subtract(number1 - number2)
-Sum of 10 and 5 is 15
-Difference of 10 and 5 is 5
-Product of 10 and 5 is 50
+# Print results
+print("Sum of", number1, "and", number2, "is", sum_result)
+print("Difference of", number1, "and", number2, "is", difference)
+print("Product of", number1, "and", number2, "is", product)
+
