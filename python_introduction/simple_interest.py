2facf00562e1d2e72fecef8a3998b14475cf8e68
Ajumoke-1isiodu <georgiaisiodu@gmail.com>
2025-09-11 15:37:37 +0100

    updated correction

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,9 @@
 # Simple Interest Calculation
 
-principal(P) = 1000  # principal in dollars
-rate(R) = 0.05       # rate (5%)
-time(T) = 3          # time in years
+principal = 1000  # principal in dollars
+rate = 0.05       # rate (5%)
+time = 3          # time in years
+
 
 #calculating simple interest
 interest = principal * rate * time
