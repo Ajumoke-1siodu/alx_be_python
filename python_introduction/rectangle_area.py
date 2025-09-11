e56de893eef1c373d6618abc5c5c5d0b31e34506
Ajumoke-1isiodu <georgiaisiodu@gmail.com>
2025-09-11 16:06:40 +0100

    the change

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,5 +3,5 @@ length = 10
 width = 5
 
 #calculating area of the rectangle
-Area = length * width   # store tge result in Area
-print("The area of the rectangle is",Area)
+area = length * width   # store the result in Area
+print("The area of the rectangle is",area)
