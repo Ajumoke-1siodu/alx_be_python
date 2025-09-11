a547a00434c663cac26353cc70b77833b3f52968
Ajumoke-1isiodu <georgiaisiodu@gmail.com>
2025-09-11 15:52:16 +0100

    add rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+#assigning values to variable
+length = 10
+width = 5
+
+#calculating area of the rectangle
+Area = length * width   # store tge result in Area
+print("The area of the rectangle is",Area)
