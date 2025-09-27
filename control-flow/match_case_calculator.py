d7eab76068298725ee0f3e9b77cef603b1ca23c1
Ajumoke_1siodu <georgiaisiodu@yahoo.com>
2025-09-27 11:01:56 +0100

    add match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, / ): )
+
+match operations:
+    case "+":
+    result = num1 + num2
+        print("The result is [result].")
+         case "-":
+         result = num1 - num2
+         print("The result is [result].")
+             case "*":
+             result = num1 * num2
+             print("The resul is [result].")
+                 case "/"
+                 result = num1 / num2
+                 print("The result is [result].)
+                     case _:
+                     print("Invalid day entered.")
